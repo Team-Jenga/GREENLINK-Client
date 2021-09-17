64256cd80967a8cb485088f1ef0a6364cf9d1893
WooYong Kim <wykim970@gmail.com>
2021-09-18 01:49:00 +0900

    Modify Login.js (로그인, 회원가입 분리)

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,8 +2,38 @@ import React, { Component } from 'react';
 import Navbar from './Navbar/Navbar';
 import '../css/Login.css';
 
+import axios from "axios";
+
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            member_id: "",
+            member_pw: ""
+        };
+    };
+
+    idChange = (e) => {this.setState({member_id: e.target.value})};
+    pwChange = (e) => {this.setState({member_pw: e.target.value})};
+
+    onClickLogin = () => {
+        if (this.state.member_id != "" &&
+            this.state.member_pw != "") {
+                axios.post("http://ec2-52-78-154-227.ap-northeast-2.compute.amazonaws.com/api/signin", {
+                    member_id: this.state.member_id,
+                    member_pw: this.state.member_pw
+                }).then(function (response) {
+                    console.log(response);
+                }).catch(function (error) {
+                    console.log(error);
+                }).then(alert('로그인 성공'));
+            } else {
+                alert("실패!");
+            };
+    };
+
     render() {
+        console.log(this.state);
         return(
             <div>
                 <Navbar/>
@@ -11,13 +41,13 @@ class Login extends Component {
                     <h3>Log in</h3>
 
                     <div className="form-group">
-                        <label>Email</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
+                        <label>Id</label>
+                        <input type="text" className="form-control" placeholder="Enter email" onChange={this.idChange}/>
                     </div>
 
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
+                        <input type="text" className="form-control" placeholder="Enter password" onChange={this.pwChange}/>
                     </div>
 
                     <div className="form-group">
@@ -27,63 +57,9 @@ class Login extends Component {
                         </div>
                     </div>
 
-                    <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                    <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={this.onClickLogin}>Sign in</button>
                     <p className="forgot-password text-right">
                         Forgot <a href="#">password?</a>
-                    </p>
-                </div>
-
-                <div className="register-form">
-                    <h3>Register</h3>
-
-                    <div className="form-group">
-                        <label>아이디</label>
-                        <input type="text" className="form-control" placeholder="First name" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>비밀번호</label>
-                        <input type="text" className="form-control" placeholder="Last name" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>이름</label>
-                        <input type="text" className="form-control" placeholder="Enter email" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>닉네임</label>
-                        <input type="text" className="form-control" placeholder="Enter password" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>생년월일</label>
-                        <input type="date" className="form-control" placeholder="Enter password" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>전화번호</label>
-                        <input type="text" className="form-control" placeholder="Enter password"/>
-                    </div>
-
-                    <div className="form-group">
-                        <label>이메일</label>
-                        <input type="email" className="form-control" placeholder="Enter password" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>거주 위치</label>
-                        <input type="text" className="form-control" placeholder="Enter password" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>가구 수</label>
-                        <input type="int" className="form-control" placeholder="Enter password" />
-                    </div>
-
-                    <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
-                    <p className="forgot-password text-right">
-                        Already registered <a href="#">log in?</a>
                     </p>
                 </div>
             </div>

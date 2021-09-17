2f2db5230c4bf9a0246a03d3f988370b5ea18024
WooYong Kim <wykim970@gmail.com>
2021-09-17 15:39:10 +0900

    Rename Navbar.js class name

--- frontend/src/components/Navbar/MenuItems.js
+++ frontend/src/components/Navbar/MenuItems.js
@@ -2,24 +2,24 @@ export const MenuItems = [
     {
         title: 'About',
         url: '/About',
-        cName: 'nav-link'
+        cName: 'my-nav-link'
     },
 
     {
         title: '캠페인 검색',
         url: '/campaign',
-        cName: 'nav-link'
+        cName: 'my-nav-link'
     },
     
     {
         title: '공지사항',
         url: '/notice',
-        cName: 'nav-link'
+        cName: 'my-nav-link'
     },
 
     {
         title: '로그인',
         url: '/login',
-        cName: 'nav-link-mobile'
+        cName: 'my-nav-link-mobile'
     }
 ]

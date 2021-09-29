d6277515bb985d34f7c1df3e05523505cb07cb87
WooYong Kim <wykim970@gmail.com>
2021-09-29 20:10:03 +0900

    Delete file

--- frontend/src/components/Campaign.js
+++ frontend/src/components/Campaign.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
-import { parsingEvent } from '../crawling/Crawling'
-
 class Campaign extends Component {
     render() {
-        parsingEvent();
         return(
         <div>
             <h1>Campagin</h1>
